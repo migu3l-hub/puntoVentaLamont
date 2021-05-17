4bbea973b58d97d5110058d300dd117b6216014a
migu3l-hub <cachecuas1@gmail.com>
2021-05-17 14:13:55 -0500

    Se cambia todo el template de uso por uno mejor, sin embargo se estan pasando todas las vistas

---
+++ static/js/dropdown.js
@@ -0,0 +1,25 @@
+function dropdownMenu() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.userDropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,66 +1,22 @@
-﻿$(document).ready(function () {
-   // Se agregan los siguientes componentes al documento:
-   $("#navegador").load('/static/navegador.html');
+/*!
+    * Start Bootstrap - SB Admin v6.0.3 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2021 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    (function($) {
+    "use strict";
 
-   function mostrarPwd(contenedor_pwd) {
-      if ($(contenedor_pwd + ' input').attr("type") == "text") {
-         $(contenedor_pwd + ' input').attr('type', 'password');
-         $(contenedor_pwd + ' a i').addClass("glyphicon-eye-open");
-         $(contenedor_pwd + ' a i').removeClass("glyphicon-eye-close")
-      } else if ($(contenedor_pwd + '  input').attr("type") == "password") {
-         $(contenedor_pwd + ' input').attr('type', 'text');
-         $(contenedor_pwd + ' a i').addClass("glyphicon-eye-close");
-         $(contenedor_pwd + ' a i').removeClass("glyphicon-eye-open")
-      }
-   }
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
 
-   var validaForm=function (form){
-      var a = $("input[type=text], input[type=password], textarea").serializeArray();
-      var r = 0;
-      a.forEach(function (el, i, ar) {
-         if (el.value === '') {
-            $("#form-"+form+" #div-" + el.name).addClass("has-error has-feedback");
-            $("#form_error").addClass("alert alert-danger alert-dismissible fade in");
-            $("#form-"+form+" #form_error").css("display", "block");
-            $("#form-"+form+" #form_error").text('No se llenaron todos los datos.');
-            r=1;
-         } else {
-            $("#form-"+form+" #div-" + el.name).removeClass("has-error has-feedback");
-         }
-      });
-      if (r==0) {
-         $("#form-"+form+" #form_error").css("display", "none");
-         $("#form-"+form+" #form_error").text("");
-      }
-      return $("#form-"+form+" #form_error").css("display")=="none";
-   }
-
-   $("#form-login").submit(function (event) {
-      if (!validaForm("login")){
-         event.preventDefault();
-      }
-   });
-
-   $("#form-usr").submit(function (event) {
-      if (!validaForm("usr")){
-         event.preventDefault();
-      }
-   });
-
-   $("#form-srv").submit(function (event) {
-      if (!validaForm("srv")){
-         event.preventDefault();
-      }
-   });
-
-   $("#mtr_conf_pwd a").click(function (event) {
-      event.preventDefault()
-      mostrarPwd('#mtr_conf_pwd');
-   });
-
-   $('#mtr_pwd a').click(function (event) {
-      event.preventDefault()
-      mostrarPwd('#mtr_pwd');
-   });
-   
-});
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
