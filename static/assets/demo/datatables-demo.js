b2e33b719efbf3602d9efe249a6dfcf290cb5a71
migu3l-hub <cachecuas1@gmail.com>
2021-05-20 11:22:13 -0500

    Se ponen los menus de creacion y edicion de aparatos y clientes pero aun se ve feo los de eliminacion y aun falta poner imagenes en los aparatos

--- static/assets/demo/datatables-demo.js
+++ static/assets/demo/datatables-demo.js
@@ -1,4 +1,8 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    "language": {
+            url: '/static/lib/spanish.txt' // si encuentra el documento pero por alguna razon no funciona
+        },
+  });
 });
---
+++ static/js/crear_server.js
@@ -0,0 +1,40 @@
+function abrir_modal_edicion(url){
+    var $ = jQuery.noConflict();
+    $('#edicion').load(url, function (){
+      $(this).modal('show');
+    });
+  }
+
+function abrir_modal_creacion(url) {
+    var $ = jQuery.noConflict();
+    $('#creacion').load(url, function () {
+        $(this).modal('show');
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var $ = jQuery.noConflict();
+//   function abrir_modal_edicion(url){
+//     $('#edicion').load(url, function (){
+//       $(this).modal('show');
+//     });
+//   }
+//   function abrir_modal_creacion(url){
+//     $('#creacion').load(url,function(){
+//       $(this).modal('show');
+//     });
+//   }
+
